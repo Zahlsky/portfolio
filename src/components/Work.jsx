3ef82b438277462a108f8fbabf4f96991c86d09e
az85 <aaron.zahl@gmail.com>
2022-10-05 18:32:20 +0100

    Work component complete

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import BAOTY from '../assets/aoty.png'
+import CRYPTO from '../assets/cryptoscreenshot.png'
+import EARTH from '../assets/EarthBnb.png'
+import SPACE from '../assets/SpaceInvaders.png'
 
 const Work = () => {
   return (
@@ -9,18 +13,76 @@ const Work = () => {
           <p className='py-6'>Check out some of my recent work</p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            <div>
-              <span>
+        {/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-4'>
 
+          {/* Grid Item */}
+          <div style={{backgroundImage: `url(${BAOTY})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+    Django and React Application
               </span>
-              <div>
+              <div className='pt-8 text-center'>
                 <a href="/">
-                  <button></button>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-500 font-bold text-lg'>Demo</button>
                 </a>
                 <a href="/">
-                  <button></button>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-500 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div style={{backgroundImage: `url(${EARTH})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+    MERN Stack Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-500 font-bold text-lg'>Demo</button>
+                </a>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-500 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: `url(${CRYPTO})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+    React Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-500 font-bold text-lg'>Demo</button>
+                </a>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-500 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div style={{backgroundImage: `url(${SPACE})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+    JavaScript Game
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-500 font-bold text-lg'>Demo</button>
+                </a>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-500 font-bold text-lg'>Code</button>
                 </a>
               </div>
             </div>
